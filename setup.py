8af6c609f1e5ddcd3b0a49fffbccc26306614884
Steven Troxler <steven@cloudlabs.io>
2015-09-11 16:44:02 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='pyvzutil',
-      version='0.0.5',
+      version='0.0.6',
       description='Utilities for working with openvz clusters',
       long_description=readme(),
       classifiers=[
